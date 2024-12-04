bc373eee719f40785f8d4953e20f507286fdcad5
Timur Khananov <khananovtf@gmail.com>
2024-12-05 00:22:46 +0300

    feat: detect language

--- src/main/java/ru/khananov/services_translator/services/impl/TranslateServiceImpl.java
+++ src/main/java/ru/khananov/services_translator/services/impl/TranslateServiceImpl.java
@@ -14,7 +14,6 @@ import java.util.Collections;
 @Service
 public class TranslateServiceImpl implements TranslateService {
   private static final String YA_API_TRANSLATE_URL = "https://translate.api.cloud.yandex.net/translate/v2/translate";
-  private static final String TARGET_LANGUAGE = "ru";
   private final RestTemplate restTemplate;
 
   @Value("${ya.api.folder-id}")
@@ -30,13 +29,29 @@ public class TranslateServiceImpl implements TranslateService {
   public String translate(String text) {
     if (text == null || text.isEmpty()) return "";
 
+    String targetLanguage = detectLanguage(text);
     HttpHeaders headers = new HttpHeaders();
     headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
     headers.set(HttpHeaders.AUTHORIZATION, "Api-Key " + apiKey);
-    Records.TranslateRequest request = new Records.TranslateRequest(TARGET_LANGUAGE, text, folderId);
+    Records.TranslateRequest request = new Records.TranslateRequest(targetLanguage, text, folderId);
     HttpEntity<Records.TranslateRequest> entity = new HttpEntity<>(request, headers);
     Records.TranslateResponse input = restTemplate.postForObject(YA_API_TRANSLATE_URL, entity, Records.TranslateResponse.class);
     if (input == null || input.translations() == null || input.translations().isEmpty()) return "Ошибка перевода";
     return input.translations().getFirst().text();
+  }
+
+  private String detectLanguage(String text) {
+    if (text == null || text.isEmpty()) return "";
+
+    boolean hasCyrillic = text.chars().anyMatch(c -> Character.UnicodeBlock.of(c) == Character.UnicodeBlock.CYRILLIC);
+    boolean hasLatin = text.chars().anyMatch(c -> Character.UnicodeBlock.of(c) == Character.UnicodeBlock.BASIC_LATIN);
+
+    if (hasCyrillic && !hasLatin) {
+      return "en";
+    } else if (hasLatin && !hasCyrillic) {
+      return "ru";
+    } else {
+      return "ru";
+    }
   }
 }
